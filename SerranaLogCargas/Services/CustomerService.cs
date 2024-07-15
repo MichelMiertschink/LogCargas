af50c13de69d9d5d37bc8be1dd804d3435f9a09a
Michel Miertschink <chereu.m@gmail.com>
2024-07-14 22:01:35 -0300

    Customers import, not validate exists

--- SerranaLogCargas/Services/CustomerService.cs
+++ SerranaLogCargas/Services/CustomerService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using LogCargas.Data;
 using LogCargas.Models;
+using System.ComponentModel;
+using OfficeOpenXml;
 
 namespace LogCargas.Services
 {
@@ -26,5 +28,72 @@ namespace LogCargas.Services
             _context.Add(obj);
             await _context.SaveChangesAsync();
         }
+
+        // Importar arquivo de clientes (Excel)
+        public MemoryStream LerStream(IFormFile formFile)
+        {
+            using (var stream = new MemoryStream())
+            {
+                formFile?.CopyTo(stream);
+                var ListBytes = stream.ToArray();
+                return new MemoryStream(ListBytes);
+            }
+        }
+
+        public List<Customer> LerXls(MemoryStream stream)
+        {
+            try
+            {
+                var resposta = new List<Customer>();
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (ExcelPackage package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    int numeroLinhas = worksheet.Dimension.End.Row;
+
+                    for (int linha = 2; linha <= numeroLinhas; linha++)
+                    {
+                        var customer = new Customer();
+
+                        if (worksheet.Cells[linha, 1].Value != null
+                            && worksheet.Cells[linha, 2].Value != null)
+                        {
+                            customer.Id = 0;
+                            customer.CNPJ = worksheet.Cells[linha, 1].Value.ToString();
+                            customer.CorporateReason = worksheet.Cells[linha, 2].Value.ToString();
+                            customer.CostCenter = worksheet.Cells[linha, 3].Value.ToString();
+
+                            resposta.Add(customer);
+                        }
+                    }
+                }
+
+                return resposta;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void SalvarImportacao(List<Customer> customers)
+        {
+            try
+            {
+                foreach (var customer in customers)
+                {
+                    _context.Add(customer);
+                    _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
     }
 }
+

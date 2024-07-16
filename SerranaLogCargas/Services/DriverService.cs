4cc86c339708a7a5dce8860ce0c8047d32e7aa5c
Michel Miertschink <68962017+MichelMiertschink@users.noreply.github.com>
2024-07-16 13:48:21 -0300

    Add files via upload

--- SerranaLogCargas/Services/DriverService.cs
+++ SerranaLogCargas/Services/DriverService.cs
@@ -3,6 +3,7 @@ using LogCargas.Data;
 using LogCargas.Models;
 using LogCargas.Services.Exceptions;
 using System.Linq.Expressions;
+using OfficeOpenXml;
 
 namespace LogCargas.Services
 {
@@ -55,6 +56,83 @@ namespace LogCargas.Services
             var driver = _context.Driver.Find(id);
             _context.Driver.Remove(driver);
             await _context.SaveChangesAsync();
+        }
+
+        // Importar arquivo de motoristas (Excel)
+        public MemoryStream LerStream(IFormFile formFile)
+        {
+            using (var stream = new MemoryStream())
+            {
+                formFile?.CopyTo(stream);
+                var ListBytes = stream.ToArray();
+                return new MemoryStream(ListBytes);
+            }
+        }
+
+        // Ler a stream do arquivo e criar a lista
+        public List<Driver> LerXls(MemoryStream stream)
+        {
+            try
+            {
+                var resposta = new List<Driver>();
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (ExcelPackage package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    int numeroLinhas = worksheet.Dimension.End.Row;
+
+                    for (int linha = 2; linha <= numeroLinhas; linha++)
+                    {
+                        var driver = new Driver();
+
+                        if (worksheet.Cells[linha, 1].Value != null
+                            && worksheet.Cells[linha, 2].Value != null)
+                        {
+                            driver.Id = 0;
+                            driver.Name = worksheet.Cells[linha, 1].Value.ToString();
+                            driver.CelPhone = worksheet.Cells[linha, 2].Value.ToString();
+                            driver.CPF = worksheet.Cells[linha, 3].Value.ToString();
+
+                            resposta.Add(driver);
+                        }
+                    }
+                }
+
+                return resposta;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Salvar a importação, se não existir o CNPJ
+        public async Task SalvarImportacao(List<Driver> drivers)
+        {
+            try
+            {
+                foreach (var driver in drivers)
+                {
+                    bool hasAny = await  _context.Driver.AnyAsync(x => x.CPF == driver.CPF);
+                    if (!hasAny)
+                    {
+                        _context.Add(driver);
+                        await _context.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        _context.Update(driver);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
         }
     }
 }

aba9767fec4c44974e0ffc3ab5afb4550358a0b7
Michel Miertschink <chereu.m@gmail.com>
2024-09-13 15:20:48 -0300

    Under Construction

---
+++ SerranaLogCargas/Controllers/RedeFrotaController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.Mvc;
+using LogCargas.Models;
+using LogCargas.Services;
+using Microsoft.AspNetCore.Mvc;
+using ReflectionIT.Mvc.Paging;
+
+namespace LogCargas.Controllers
+{
+    public class RedeFrotaController : Controller
+    {
+        private readonly RedeFrotaService _redeFrotaService;
+
+        public RedeFrotaController(RedeFrotaService redeFrota)
+        {
+            _redeFrotaService = redeFrota;
+        }
+
+        public async Task<IActionResult> Index(string filter, int pageindex = 1, string sort = "dataTransacao")
+        {
+            var resultado = await _redeFrotaService.FindForAll(filter);
+
+            var model = await PagingList.CreateAsync(resultado.Result, 5, pageindex, sort, "dataTransacao");
+            model.RouteValue = new RouteValueDictionary { { "filter", filter } };
+            return View(model);
+        }
+    }
+}
---
+++ SerranaLogCargas/Services/RedeFrotaService.cs
@@ -0,0 +1,36 @@
+﻿using LogCargas.Data;
+using LogCargas.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LogCargas.Services
+{
+    public class RedeFrotaService
+    {
+
+        private readonly LogCargasContext _context;
+
+        public RedeFrotaService(LogCargasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IQueryable<RedeFrota>> FindForAll(string filter)
+        {
+            var resultado = _context.RedeFrota.AsNoTracking().AsQueryable();
+            if (!string.IsNullOrEmpty(filter))
+            {
+                resultado = resultado.Where(r => r.Placa.Contains(filter) ||
+                                                 r.codigoTransacao.ToString().Contains(filter));
+            }
+            return resultado.AsQueryable();
+        }
+
+        public async Task InsertAsync(RedeFrota redeFrota)
+        {
+            redeFrota.IncludeDate = DateTime.Now;
+            _context.Add(redeFrota);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
